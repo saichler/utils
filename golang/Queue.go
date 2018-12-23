5c1a026fb314e395c42309b9b28c69d68c2103e6
Sharon Aicler <saichler@gmail.com>
2018-12-23 09:06:50 -0800

    Queue impl

---
+++ golang/Queue.go
@@ -0,0 +1,40 @@
+package dsutils
+
+import "sync"
+
+type Queue struct {
+	inbox []interface{}
+	lock *sync.Cond
+}
+
+func NewQueue() *Queue {
+	q:=&Queue{}
+	q.inbox = make([]interface{},0)
+	q.lock = sync.NewCond(&sync.Mutex{})
+	return q
+}
+
+func (q *Queue) Push(any interface{}) error {
+	q.lock.L.Lock()
+	defer q.lock.L.Unlock()
+	q.inbox = append(q.inbox,any)
+	q.lock.Broadcast()
+	return nil
+}
+
+func (q *Queue) Pop() interface{} {
+	q.lock.L.Lock()
+	defer q.lock.L.Unlock()
+
+	var elem interface{}
+	for ;elem==nil; {
+		if len(q.inbox)==0 {
+			q.lock.Wait()
+		} else {
+			elem = q.inbox[0]
+			q.inbox=q.inbox[1:]
+		}
+	}
+
+	return elem
+}
