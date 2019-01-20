ff4f88d0da6607f6597e5a905303075c7d971800
Sharon Aicler <saichler@gmail.com>
2019-01-20 10:43:13 -0800

    Trim

---
+++ golang/tests/Trim_test.go
@@ -0,0 +1,14 @@
+package dbutil
+
+import (
+	"github.com/saichler/utils/golang"
+	"testing"
+)
+
+func TestTrim(t *testing.T) {
+	input:="\r\n  \n\rHello World \r\n \n\r"
+	output:=utils.Trim(input)
+	if output!="Hello World" {
+		t.Fail()
+	}
+}
