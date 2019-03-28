41de229e0ae64c6262333e5024d3fe5583d90334
Sharon Aicler <saichler@gmail.com>
2019-03-28 06:53:11 -0700

    Start Marshal

--- golang/orm/common/Common.go
+++ golang/orm/common/Common.go
@@ -1,7 +1,5 @@
 package common
 
-import "strings"
-
 const (
 	TITLE          = "Title"
 	SIZE           = "Size"
@@ -12,12 +10,6 @@ const (
 	NON_UNIQUE_KEY = "NonUniqueKey"
 )
 
-func GetTag(tag, tags string) string {
-	index := strings.Index(tags, tag+":")
-	if index == -1 {
-		return ""
-	}
-	subset := tags[index+len(tag)+2:]
-	index = strings.Index(subset, "\"")
-	return subset[0:index]
+type Persistency interface {
+
 }
---
+++ golang/orm/marshal/Marshal.go
@@ -0,0 +1,11 @@
+package marshal
+
+import (
+	. "github.com/saichler/utils/golang/orm/common"
+	. "github.com/saichler/utils/golang/orm/registry"
+	. "github.com/saichler/utils/golang/orm/transaction"
+)
+
+func Marshal(any interface{},r *OrmRegistry,tx *Transaction, pr Persistency) error {
+
+}
