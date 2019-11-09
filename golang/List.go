e589e441fcb99b8326be64afff6e449dedfc3f6f
Sharon Aicler <saichler@gmail.com>
2019-11-09 14:53:08 -0500

    Add Log To Console

--- golang/List.go
+++ golang/List.go
@@ -24,7 +24,34 @@ func (l *List) Add(any interface{}) {
 func (l *List) Get(pos int) interface{} {
 	l.lock.L.Lock()
 	defer l.lock.L.Unlock()
+	if pos < 0 || len(l.internalList) <= pos {
+		return nil
+	}
 	return l.internalList[pos]
+}
+
+func (l *List) Del(pos int) interface{} {
+	l.lock.L.Lock()
+	defer l.lock.L.Unlock()
+	if pos < 0 || len(l.internalList) <= pos {
+		return nil
+	}
+	elem := l.internalList[pos]
+	aside := l.internalList[0:pos]
+	zside := l.internalList[pos+1:]
+	l.internalList = aside
+	l.internalList = append(l.internalList, zside...)
+	return elem
+}
+
+func (l *List) List(pos int) interface{} {
+	l.lock.L.Lock()
+	defer l.lock.L.Unlock()
+	result := make([]interface{}, len(l.internalList))
+	for i, v := range l.internalList {
+		result[i] = v
+	}
+	return result
 }
 
 func (l *List) Size() int {
--- golang/Log.go
+++ golang/Log.go
@@ -3,6 +3,9 @@ package utils
 import (
 	"errors"
 	log "github.com/sirupsen/logrus"
+	"net"
+	"strings"
+	"sync"
 )
 
 func Fatal(any ...interface{}) {
@@ -28,4 +31,45 @@ func Debug(any ...interface{}) {
 
 func Trace(any ...interface{}) {
 	log.Trace(any)
+}
+
+var conn net.Conn
+var mtx = &sync.Mutex{}
+var lineFeed = []byte("\n")
+
+func SetConsoleConnection(connection net.Conn) {
+	mtx.Lock()
+	defer mtx.Unlock()
+	conn = connection
+}
+
+func Print(msg string) {
+	mtx.Lock()
+	defer mtx.Unlock()
+	if conn != nil {
+		conn.Write([]byte(msg))
+	}
+}
+
+func Println(msg string) {
+	mtx.Lock()
+	defer mtx.Unlock()
+	if conn != nil {
+		conn.Write([]byte(msg))
+		conn.Write(lineFeed)
+	}
+}
+
+func Read() (string, error) {
+	if conn == nil {
+		return "", nil
+	}
+	line := make([]byte, 4096)
+	n, e := conn.Read(line)
+	if e != nil {
+		e = Error("Failed to read line:", e)
+		return "", e
+	}
+	inputLine := strings.TrimSpace(string(line[0:n]))
+	return inputLine, nil
 }
